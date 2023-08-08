e74e176283dd8235a17ad82e89a94b96ef118a7e
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2023-08-08 09:19:21 +0000

    1.2.0

---
+++ src/jp/f_matano44/mreccorpus2/ScriptsManager.java
@@ -0,0 +1,142 @@
+/*
+ * mRecCorpus2
+ * Copyright (C) 2023  Fumiyoshi MATANO
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package jp.f_matano44.mreccorpus2;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+
+class ScriptsManager extends JPanel {
+    private final RecorderBody recorder;
+    private final int[] currentIndex;
+    private final int textAreaWidth = 40;
+    private final List<String> lines;
+
+    public final JLabel indexLabel;
+    public final JTextArea scriptViewer;
+    public final JTextArea saveToViewerArea;
+    public final JTextArea scriptsPathViewerArea;
+
+    public ScriptsManager(
+        RecorderBody recorder, AppConfig conf, int[] currentIndex
+    ) {
+        this.recorder = recorder;
+        this.currentIndex = currentIndex;
+
+        // Index label
+        this.indexLabel = new JLabel();
+        this.indexLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        // Script viewer
+        this.scriptViewer = new JTextArea();
+        setTextAreaSetting(this.scriptViewer);
+        this.scriptViewer.setBorder(
+            new LineBorder(Color.BLACK, 1, true)
+        );
+        this.scriptViewer.setColumns(textAreaWidth);
+        this.scriptViewer.setRows(7); // ここの数字は決め打ち
+
+        // saveTo Panel
+        this.saveToViewerArea = new JTextArea();
+        setTextAreaSetting(this.saveToViewerArea);
+        this.saveToViewerArea.setColumns(textAreaWidth);
+
+        // Scripts loader
+        final String scriptPathString = conf.scripts.getAbsolutePath();
+        this.lines = new ArrayList<>();
+        readFile(scriptPathString);
+        this.scriptsPathViewerArea = new JTextArea(
+            "Scripts:  " + scriptPathString
+        );
+        setTextAreaSetting(this.scriptsPathViewerArea);
+        this.scriptsPathViewerArea.setColumns(textAreaWidth);
+        this.scriptsPathViewerArea.setBorder(
+            new EmptyBorder(0, 0, 5, 0)
+        );
+
+        // set default value
+        this.updateText();
+    }
+
+    public void nextLine() {
+        currentIndex[0]++;
+        if(lines.size() <= currentIndex[0]) {
+            currentIndex[0] = 0;
+        }
+        updateText();
+    }
+
+    public void prevLine() {
+        if (currentIndex[0] <= 0) {
+            currentIndex[0] = lines.size();
+        }
+        currentIndex[0]--;
+        updateText();
+    }
+
+    private void updateText() {
+        final int num = currentIndex[0] + 1;
+        final String saveToString = recorder.getSavePath(num).getAbsolutePath();
+        indexLabel.setText(String.valueOf(num));
+        scriptViewer.setText(lines.get(currentIndex[0]));
+        saveToViewerArea.setText("Save to:  " + saveToString);
+    }
+
+    private void readFile(final String filePath) {
+        try (final var sc = new Scanner(new File(filePath))) {
+            while (sc.hasNextLine()) {
+                final String[] parts = sc.nextLine().split("[\t,:]");
+                final StringBuilder sb = new StringBuilder();
+                for (final String temp : parts) {
+                    sb.append(temp + "\n");
+                }
+                lines.add(sb.toString());
+            }
+        } catch (IOException e) {
+            final String errorString = "Error in reading file";
+            lines.clear();
+            lines.add(errorString);
+        }
+
+        if (!lines.isEmpty()) {
+            updateText();
+        } else {
+            scriptViewer.setText("No lines in the file");
+        }
+    }
+
+    private static void setTextAreaSetting(JTextArea textArea) {
+        textArea.setWrapStyleWord(true);   
+        textArea.setLineWrap(true);        
+        textArea.setEditable(false);   
+        textArea.setBackground(null);
+        textArea.setBorder(null);
+    }
+}
